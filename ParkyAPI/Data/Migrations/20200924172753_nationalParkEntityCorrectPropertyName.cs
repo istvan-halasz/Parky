49d9bf27867f86f6103f37311fd5fb52a49f6d13
Istvan Halasz <ihalasz@pentalog.com>
2020-09-27 19:24:35 +0300

    upload project

---
+++ ParkyAPI/Data/Migrations/20200924172753_nationalParkEntityCorrectPropertyName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParkyAPI.Migrations
+{
+    public partial class nationalParkEntityCorrectPropertyName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estabilished",
+                table: "NationalParks");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Established",
+                table: "NationalParks",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Established",
+                table: "NationalParks");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Estabilished",
+                table: "NationalParks",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
---
+++ ParkyWeb/Models/ViewModel/IndexVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ParkyWeb.Models.ViewModel
+{
+    public class IndexVM
+    {
+        public IEnumerable<NationalPark> NationalParkList { get; set; }
+        public IEnumerable<Trail> TrailList { get; set; }
+    }
+}
